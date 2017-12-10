259a77c5f904544b5d1df9e72d40c6565e3c66a3
Fred Willmore <fredwillmore@gmail.com>
2017-12-09 19:44:02 -0800

    implemented PercolationStats

--- src/Percolation.java
+++ src/Percolation.java
@@ -1,172 +1,183 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-
 public class Percolation {
-    private class Site {
-        private Integer col;
-        private Integer row;
-        private Boolean open;
-        private Boolean full;
+    private final int n;
+    private final Site[] sites;
+    private boolean percolates;
+    private final WeightedQuickUnionUF uf;
+    private int openSites = 0;
+//    private int calls=0;
 
-        public Site(Integer row, Integer col) {
+    public Percolation(int n) {
+        if (n <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        this.n = n;
+        sites = new Site[n * n];
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                sites[getPosition(row, col)] = new Site(row, col);
+            }
+        }
+        percolates = false;
+        uf = new WeightedQuickUnionUF((int) Math.pow(n, 2));
+
+    }
+
+    private void fill(Site currentSite) {
+        currentSite.setFull(true);
+        for (Site neighbor: getNeighbors(currentSite)) {
+            if (neighbor != null && neighbor.isOpen() && !neighbor.isFull()) {
+                fill(neighbor);
+            }
+        }
+    }
+
+    private Site[] getNeighbors(Site currentSite) {
+        int row = currentSite.getRow();
+        int col = currentSite.getCol();
+        Site[] neighbors = {
+                getSiteAtPosition(row, col-1),
+                getSiteAtPosition(row, col+1),
+                getSiteAtPosition(row-1, col),
+                getSiteAtPosition(row+1, col)
+        };
+        return neighbors;
+    }
+
+    public void open(int row, int col) {
+        testRange(row, col);
+        Site site = getSiteAtPosition(row, col);
+        if (!site.isOpen()) {
+            site.setOpen(true);
+            openSites++;
+            Site[] neighbors = getNeighbors(site);
+            for (Site neighbor: neighbors) {
+                if (neighbor != null && neighbor.isOpen()) {
+                    site.setOpen(true);
+                    uf.union(getPosition(row, col), getPosition(neighbor.getRow(), neighbor.getCol()));
+                }
+            }
+            boolean fillSite = row == 1;
+            for (Site neighbor: neighbors) {
+                fillSite |= (neighbor != null && neighbor.isFull());
+            }
+            if(fillSite) {
+                fill(site);
+            }
+        }
+    }
+
+    public boolean isOpen(int row, int col) {
+        testRange(row, col);
+        Site site = getSiteAtPosition(row, col);
+        return site.isOpen();
+    }
+
+    public boolean isFull(int row, int col) {
+        testRange(row, col);
+        Site currentSite = getSiteAtPosition(row, col);
+        if (currentSite.isFull()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean percolates() {
+        if(percolates) return true;
+        int row = n;
+        for (int col = 1; col <= n; col++) {
+            if (isFull(row, col)) {
+                percolates = true;
+                return true;
+            }
+        }
+        return percolates;
+    }
+
+    public int numberOfOpenSites() {
+        return openSites;
+    }
+
+    private void testRange(int row, int col) {
+        if (!(row > 0 && row <= n) || !(col > 0 && col <= n))
+            throw new java.lang.IllegalArgumentException();
+    }
+
+
+    private int getPosition(int row, int col) {
+        if (row > 0 && row <= n && col > 0 && col <= n) {
+            return (row - 1) * n + (col - 1);
+        }
+        return -1;
+    }
+
+    private Site getSiteAtPosition(int row, int col) {
+        int position = getPosition(row, col);
+
+        if (position >= 0) {
+            return sites[position];
+        }
+        return null;
+
+    }
+
+    private static class Site {
+        private final int col;
+        private final int row;
+        private boolean open;
+        private boolean full;
+
+        public Site(int row, int col) {
             this.row = row;
             this.col = col;
             this.open = false;
-            this.full = false;
         }
 
-        public Integer getCol() {
+        public int getCol() {
             return col;
         }
 
-        public void setCol(Integer col) {
-            this.col = col;
-        }
-
-        public Integer getRow() {
+        public int getRow() {
             return row;
         }
 
-        public void setRow(Integer row) {
-            this.row = row;
-        }
-
-        public Boolean getOpen() {
+        public boolean isOpen() {
             return open;
         }
 
-        public void setOpen(Boolean open) {
+        public void setOpen(boolean open) {
             this.open = open;
         }
 
-        public Boolean getFull() {
+        public boolean isFull() {
             return full;
         }
 
-        public void setFull(Boolean full) {
+        public void setFull(boolean full) {
             this.full = full;
         }
-    }
-
-    private class Grid {
-        private Site[] sites;
-        private boolean percolates;
-        private WeightedQuickUnionUF uf;
-        private int n;
-
-        private int getPosition(int row, int col) {
-            if(row>0 && row<=n && col>0 && col<=n) {
-                return (row - 1) * n + (col - 1);
-            }
-            return -1;
-        }
-
-        private Site getSiteAtPosition(int row, int col) {
-            int position = getPosition(row, col);
-
-            if(position >= 0) {
-                return sites[position];
-            }
-            return null;
-
-        }
-
-        public Grid(int n) {
-            if(n < 0) {
-                throw new java.lang.IllegalArgumentException();
-            }
-            // create n-by-n grid, with all sites blocked
-            this.n = n;
-            int numSites = n*n;
-            sites = new Site[numSites];
-            for(int row = 1; row <= n; row++) {
-                for(int col = 1; col <= n; col++) {
-                    sites[getPosition(row, col)] = new Site(row, col);
-                }
-            }
-            percolates = false;
-            uf = new WeightedQuickUnionUF((int)Math.pow(n, 2));
-        }
-
-        public Boolean isFull(int row, int col) {
-            testRange(row, col);
-            if(getSiteAtPosition(row, col).getOpen()){
-                for(int i=1; i<=n; i++){
-                    if(getSiteAtPosition(1, i).getOpen()){
-                        if(uf.connected(getPosition(1, i), getPosition(row, col))){
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-
-        public void open(int row, int col) {
-            testRange(row, col);
-            Site site = getSiteAtPosition(row, col);
-            if(!site.getOpen()){
-                site.setOpen(true);
-                if(row == 1) {
-                    site.setFull(true);
-                }
-                Site[] neighbors = new Site[4];
-                neighbors[0] = getSiteAtPosition(row, col-1);
-                neighbors[1] = getSiteAtPosition(row, col+1);
-                neighbors[2] = getSiteAtPosition(row-1, col);
-                neighbors[3] = getSiteAtPosition(row+1, col);
-                for(Site neighbor: neighbors){
-                    if(neighbor != null && neighbor.getOpen()){
-                        site.setOpen(true);
-                        uf.union(getPosition(row, col), getPosition(neighbor.getRow(), neighbor.getCol()));
-                    }
-                }
-                if(row==n) {
-                    percolates = site.getFull();
-                }
-            }
-        }
-
-        public boolean isOpen(int row, int col) {
-            testRange(row, col);
-            Site site = getSiteAtPosition(row, col);
-            return site.getOpen();
-        }
-        public boolean percolates() {
-            return percolates;
-        }
-
-        private void testRange(int row, int col){
-            if(!(row > 0 && row <= n) || !(col > 0 && col <= n)) {
-                throw new java.lang.IllegalArgumentException();
-            }
-        }
 
     }
 
-    private Grid grid;
+//    public boolean isFull(int row, int col) { // is site (row, col) full?
+//        return grid.isFull(row, col);
+//    }
 
-    public Percolation(int n) {
-        grid = new Grid(n);
-    }
+//    public void open(int row, int col) {   // open site (row, col) if it is not open already
+//        grid.open(row, col);
+//    }
 
-    public boolean isFull(int row, int col) { // is site (row, col) full?
-        return grid.isFull(row, col);
-    }
+//    public boolean isOpen(int row, int col) { // is site (row, col) open?
+//        return grid.isOpen(row, col);
+//    }
 
-    public void open(int row, int col) {   // open site (row, col) if it is not open already
-        grid.open(row, col);
-    }
+//    public int numberOfOpenSites() {
+//        return grid.numberOfOpenSites();
+//    }
 
-    public boolean isOpen(int row, int col) { // is site (row, col) open?
-        return grid.isOpen(row, col);
-    }
-
-    public     int numberOfOpenSites()       // number of open sites
-
-    public boolean percolates() {             // does the system percolate?
-        return grid.percolates();
-    }
+//    public boolean percolates() {             // does the system percolate?
+//        return grid.percolates();
+//    }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -1,39 +1,78 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 public class PercolationStats {
 
-    public PercolationStats(int n, int trials) {    // perform trials independent experiments on an n-by-n grid
-        if(n < 0 || trials < 0) {
-            throw new java.lang.IllegalArgumentException();
-        }
-        double[] results = double[trials];
-        for(int i = 0; i < trials; i++){
-            Percolation percolation = new Percolation();
-            while !percolation.percolates() {
+    private final double[] results;
+    private final int trials;
+    private final int n;
+    private double meand;
+    private double stddevd;
 
-            }
-            results[i] = percolation.nu
-        }
+    public PercolationStats(int n, int trials) {    // perform trials independent experiments on an n-by-n grid
+        if (n <= 0 || trials <= 0)
+            throw new java.lang.IllegalArgumentException();
+        this.trials = trials;
+        this.n = n;
+        this.results = runTrials();
     }
 
     public double mean() {                          // sample mean of percolation threshold
-        return 0;
+        meand = meand > 0 ? meand : StdStats.mean(results);
+        return meand;
     }
 
     public double stddev() {                        // sample standard deviation of percolation threshold
-        return 0;
+        stddevd = stddevd > 0 ? stddevd : StdStats.stddev(results);
+        return stddevd;
     }
 
     public double confidenceLo() {                  // low  endpoint of 95% confidence interval
-        return 0;
+        return mean() - confidenceInterval(stddev());
     }
 
     public double confidenceHi() {                  // high endpoint of 95% confidence interval
-        return 0;
+        return mean() + confidenceInterval(stddev());
+    }
+
+    private double confidenceInterval(double stddev) {
+        return 1.96 * stddev / Math.sqrt(trials);
+    }
+
+    private double[] runTrials() {
+        double[] trialResults = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            trialResults[i] = runTrial();
+        }
+        return trialResults;
+    }
+
+    private void openRandomSite(Percolation percolation) {
+        int row, col;
+        do {
+            row = StdRandom.uniform(1, n + 1);
+            col = StdRandom.uniform(1, n + 1);
+        } while (percolation.isOpen(row, col));
+        percolation.open(row, col);
+    }
+
+
+    private double runTrial() {
+        Percolation percolation =  new Percolation(n);
+        while (!percolation.percolates())
+            openRandomSite(percolation);
+        return (double) percolation.numberOfOpenSites()/(n * n);
     }
 
     public static void main(String[] args) {        // test client (described below)
-        int n = Integer.valueOf(args[0]));
-        int trials = Integer.valueOf(args[1]));
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats percolationStats = new PercolationStats(n, trials);
+        double mean = percolationStats.mean();
+        double stddev = percolationStats.stddev();
 
-        PercolationStats percolationStats = PercolationStats.new(n, trials);
+        System.out.printf("mean                    = %f", mean).println();
+        System.out.printf("stddev                  = %f", stddev).println();
+        System.out.printf("95 pct confidence interval = [%f, %f]", percolationStats.confidenceLo(), percolationStats.confidenceHi()).println();
     }
 }
